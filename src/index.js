880b8e5080ad1e93b94af3f2f0f5dd7439233cad
Avez-Anwar <34218292+Avez-Anwar@users.noreply.github.com>
2021-01-19 15:11:05 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { text: "" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.handleChange} />
+        <br />
+        <h1>Text is changed here " {this.state.text} "</h1>
+      </div>
+    );
+  }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
